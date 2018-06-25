8d979337dffc446579b34be679dc56ab33683e02
janrembold <janrembold@gmx.net>
2018-06-25 09:25:30 +0200

    cleaned up code

--- example/bundle.js
+++ example/bundle.js
@@ -2,7 +2,7 @@
     'use strict';
 
     const defaults = {
-        duration: 600,
+        duration: 400,
         easing: (t, b, c, d) => {
             return -c *(t/=d)*(t-2) + b;
         },
@@ -10,64 +10,45 @@
     };
 
     const animatedScrollTo = (options) => {
+        if(Number.isInteger(options)) {
+            options = {
+                to: options
+            };
+        }
+
         const settings = Object.assign({}, defaults, options);
 
-        // console.log(settings);
-        console.log('document.body.scrollTop = ' + document.body.scrollTop);
-        console.log('document.documentElement.scrollTop = ' + document.documentElement.scrollTop);
-        console.log('window.scrollY = ' + window.scrollY);
-        console.log('window.pageYOffset = ' + window.pageYOffset);
-
-        // settings.startTime = performance.now();
         settings.startingYOffset = window.pageYOffset;
         settings.distanceYOffset = parseInt(settings.to, 10) - settings.startingYOffset;
 
         window.requestAnimationFrame(timestamp => animateScroll(settings, timestamp));
-        
-        // document.documentElement || document.body
-
-        // var start = element.scrollTop,
-        // change = to - start,
-        // currentTime = 0,
-        // increment = 20;
-        
-        // var animateScroll = function(){        
-        //     currentTime += increment;
-        //     var val = Math.easeInOutQuad(currentTime, start, change, duration);
-        //     element.scrollTop = val;
-        //     if(currentTime < duration) {
-        //         setTimeout(animateScroll, increment);
-        //     }
-        // };
-
-        // animateScroll();
-
     };
 
     function animateScroll(settings, now) {
         if(!settings.startTime) {
             settings.startTime = now;
         }
-        // console.log(settings);
 
         const currentTime = now - settings.startTime;
-        // console.log(currentTime);
-
-        const newYOffset = Math.round(settings.easing(
+        let newYOffset = Math.round(settings.easing(
             currentTime, 
             settings.startingYOffset, 
             settings.distanceYOffset,
             settings.duration
         ));
 
-        console.log(newYOffset);
+        if(currentTime < settings.duration) {
+            window.requestAnimationFrame(timestamp => animateScroll(settings, timestamp)); 
+        } else {
+            newYOffset = settings.to;
+        }
 
+        setScrollTopPosition(newYOffset);
+    }
+
+    function setScrollTopPosition(newYOffset) {
         document.documentElement.scrollTop = newYOffset;
         document.body.scrollTop = newYOffset;
-        //if(newYOffset < settings.to) {
-        if(currentTime < settings.duration && newYOffset !== settings.to) {
-            window.requestAnimationFrame(timestamp => animateScroll(settings, timestamp)); 
-        }
     }
 
     const easeInQuint = (t, b, c, d) => {
@@ -81,9 +62,7 @@
     };
 
     document.getElementById('scroll-600').onclick = function() {
-        animatedScrollTo({
-            to: 600
-        });
+        animatedScrollTo(600);
     };
 
     document.getElementById('scroll-1200').onclick = function() {
