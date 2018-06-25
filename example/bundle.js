e701437b43d3f1b57b701994bf6ba6e48abfb4b1
janrembold <janrembold@gmx.net>
2018-06-25 09:50:43 +0200

    updated with IE11 fixes

--- example/bundle.js
+++ example/bundle.js
@@ -9,47 +9,63 @@
         to: 0
     };
 
-    const animatedScrollTo = (options) => {
-        if(Number.isInteger(options)) {
-            options = {
-                to: options
+    const animatedScrollTo = (args) => {
+        if(isInteger(args)) {
+            args = {
+                to: args
             };
         }
 
-        const settings = Object.assign({}, defaults, options);
+        const options = extend(defaults, args);
 
-        settings.startingYOffset = window.pageYOffset;
-        settings.distanceYOffset = parseInt(settings.to, 10) - settings.startingYOffset;
+        options.startingYOffset = window.pageYOffset;
+        options.distanceYOffset = parseInt(options.to, 10) - options.startingYOffset;
 
-        window.requestAnimationFrame(timestamp => animateScroll(settings, timestamp));
+        window.requestAnimationFrame(timestamp => animateScroll(options, timestamp));
     };
 
-    function animateScroll(settings, now) {
-        if(!settings.startTime) {
-            settings.startTime = now;
+    const animateScroll = (options, now) => {
+        if(!options.startTime) {
+            options.startTime = now;
         }
 
-        const currentTime = now - settings.startTime;
-        let newYOffset = Math.round(settings.easing(
+        const currentTime = now - options.startTime;
+        let newYOffset = Math.round(options.easing(
             currentTime, 
-            settings.startingYOffset, 
-            settings.distanceYOffset,
-            settings.duration
+            options.startingYOffset, 
+            options.distanceYOffset,
+            options.duration
         ));
 
-        if(currentTime < settings.duration) {
-            window.requestAnimationFrame(timestamp => animateScroll(settings, timestamp)); 
+        if(currentTime < options.duration) {
+            window.requestAnimationFrame(timestamp => animateScroll(options, timestamp)); 
         } else {
-            newYOffset = settings.to;
+            newYOffset = options.to;
         }
 
         setScrollTopPosition(newYOffset);
-    }
+    };
 
-    function setScrollTopPosition(newYOffset) {
+    const setScrollTopPosition = (newYOffset) => {
         document.documentElement.scrollTop = newYOffset;
         document.body.scrollTop = newYOffset;
-    }
+    };
+
+    const isInteger = (value) => {
+        if(Number.isInteger) {
+            return Number.isInteger(value);
+        } else {
+            return typeof value === 'number' && isFinite(value) && Math.floor(value) === value;
+        }
+    };
+
+    const extend = (defaults, options) => {
+        const extendedOptions = {};
+        for (let key in defaults) { 
+            extendedOptions[key] = options[key] || defaults[key]; 
+        }
+        return extendedOptions;
+    };
 
     const easeInQuint = (t, b, c, d) => {
         return c*(t/=d)*t*t*t*t + b;
